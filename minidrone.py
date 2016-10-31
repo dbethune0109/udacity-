065f5084a69863c232315f21f3e688f9115f66ec
David Bethune <dbethune1970@gmail.com>
2016-10-31 12:16:30 -0700

    Add files via upload

---
+++ minidrone.py
@@ -0,0 +1,22 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen
+    #window.bgcolor("red")
+    turtle.shape("turtle")
+brad = turtle.Turtle()
+
+counter = 0
+while counter < 4:
+    brad.forward(100)
+    brad.right(90)
+    counter += 1
+
+
+    
+   
+  
+
+
+    
+draw_square()
---
+++ turtle.py
@@ -0,0 +1,14 @@
+import turtle
+
+def draw_square():
+    window = turtle.screen
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.forward(100)
+    brad.right(90)
+
+
+    window.exitonclick()
+
+draw_square()
